7be0f1bf69945a2c271d69edacb59281933a08e6
crazyxy <t-yanxue@microsoft.com>
2016-08-12 10:23:08 +0800

    add dir cmp

--- src/util.ts
+++ src/util.ts
@@ -4,6 +4,8 @@ import * as dns from "dns";
 import * as fs from "fs";
 import * as hostile from 'hostile';
 import * as child_process from 'child_process';
+import * as path from 'path';
+import * as md5file from 'md5-file';
 
 export class Util{
     public static unique(arr:any[]): any[]{
@@ -74,8 +76,63 @@ export class Util{
         }
     }
     
-    public static dircmp(dir1: string, dir2: string){
+    public static dircmp(dir1: string, dir2: string): any[]{
         let res = [];
-    }
+        if(dir1 == dir2) return res;
+        
+        let files = fs.readdirSync(dir1);
+        let sep = path.sep;
 
+        files.forEach(file=>{
+            let name1 = [dir1, file].join(sep);
+            let name2 = [dir2, file].join(sep);
+
+            let stats = fs.statSync(name1);
+            if(stats.isFile()){
+                try {
+                    if(fs.statSync(name2).isFile()){
+                        if(md5file.sync(name1) != md5file(name2)){
+                            res.push([{
+                                host: name1,
+                                cdn: name2,
+                                reason: 'MD5 Fail'
+                            }]);
+                        }
+                    }else{
+                        res.push({
+                            host: name1,
+                            cdn: name2,
+                            reason: 'Can\'t find file'
+                        })
+                    }
+                } catch (error) {
+                    res.push({
+                        host: name1,
+                        cdn: name2,
+                        reason: 'Can\'t find file'
+                    })                    
+                }
+            }else if(stats.isDirectory()){
+                try {
+                    if(fs.statSync(name2).isDirectory()){
+                        let ret = this.dircmp(name1, name2);
+                        ret.forEach(e=>res.push(e));
+                    }else{
+                        res.push({
+                            host: name1,
+                            cdn: name2,
+                            reason: 'Can\'t find directory'
+                        })
+                    }
+                } catch (error) {
+                    res.push({
+                        host: name1,
+                        cdn: name2,
+                        reason: 'Can\'t find directory'
+                    })
+                }
+            }
+        })
+        return res;
+    }
 }
